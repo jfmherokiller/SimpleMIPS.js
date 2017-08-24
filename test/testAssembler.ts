7b1acdb81fd899fa9d7209cfd79f49ce1542e425
NoahTheGoodra <peterpan0413@live.com>
2017-08-24 16:58:02 -0500

    updated the test description name
    Signed-off-by: NoahTheGoodra <peterpan0413@live.com>

--- test/testAssembler.ts
+++ test/testAssembler.ts
@@ -18,7 +18,7 @@ main:
 describe("Assembler Tests",function () {
     describe("Memory Access Opcodes",function () {
         describe("load byte test",function () {
-            it("should return correctly assembled binary code",function () {
+            it("should return correctly assembled binary code for the load byte opcode",function () {
                 let assemblout = new Assembler().assemble(ReturnTestAsm("lb $v0,0($v1)"));
                 assert.equal(assemblout.textMem[2].toString(16),"80620000");
             })
