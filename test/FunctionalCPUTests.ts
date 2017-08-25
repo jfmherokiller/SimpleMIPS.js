c97a4ea7b0931725f2b0cb39ca0772e72dcdb054
NoahTheGoodra <peterpan0413@live.com>
2017-08-25 10:23:54 -0500

    throw if cpu hits invalid instruction
    Signed-off-by: NoahTheGoodra <peterpan0413@live.com>

--- test/FunctionalCPUTests.ts
+++ test/FunctionalCPUTests.ts
@@ -1,4 +1,3 @@
-
 import {Assembler} from '../src/Assembler';
 import 'mocha';
 import {assert} from "chai";
@@ -9,21 +8,20 @@ import {Memory} from "../src/Memory";
 import {readFileSync} from "fs";
 
 
-
-describe("Functional CPU Testing",function () {
+describe("Functional CPU Testing", function () {
     let assembler;
     let mem;
     let cpu;
-    before("Setup CPU/Memory and register events",function () {
+    before("Setup CPU/Memory and register events", function () {
         assembler = new Assembler();
         mem = new Memory();
         cpu = new FunctionalCPU(mem);
         registerEvents(cpu);
     });
-    describe("hello world test",function () {
-        it("it should correctly run the hello world code",function () {
+    describe("hello world test", function () {
+        it("it should correctly run the hello world code", function () {
             let helloworld_path = __dirname + "/helloworld.asm";
-            let assembleres = assembler.assemble(readFileSync(helloworld_path,"utf8"));
+            let assembleres = assembler.assemble(readFileSync(helloworld_path, "utf8"));
             mem.importAsm(assembleres);
             cpu.reset();
             Runcpu(cpu);
@@ -34,10 +32,13 @@ describe("Functional CPU Testing",function () {
 
 function Runcpu(cpu) {
     for (let i = 0; i < 12500; i++) {
-        let exception =cpu.step();
-        if(exception === EXCEPTION_CODE.BREAK)
-        {
+        let exception = cpu.step();
+        if (exception === EXCEPTION_CODE.BREAK) {
             break;
+        }
+        if (exception === EXCEPTION_CODE.INVALID_INST)
+        {
+            throw Error(`Invalid Instruction Info:${cpu}`)
         }
     }
 }
