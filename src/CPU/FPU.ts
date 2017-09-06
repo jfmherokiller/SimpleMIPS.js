178e499ee7826bfc03f4c391ce4274dbbec08604
NoahTheGoodra <peterpan0413@live.com>
2017-09-06 16:43:01 -0500

    add function to split double
    Signed-off-by: NoahTheGoodra <peterpan0413@live.com>

--- src/CPU/FPU.ts
+++ src/CPU/FPU.ts
@@ -1,6 +1,15 @@
 
-class FPU {
+export class FPU {
     registerfile:Float32Array;
+    //function which should split the double format used by javascript into 2 single precision floats
+    split_double(input)
+    {
+        var hipart = new Float32Array([ input])[0];
+        var delta = input - hipart;
+        var lopart = new Float32Array([delta])[0];
+        return [hipart,lopart];
+    }
+
     constructor() {
         this.registerfile = new Float32Array(32);
     }
