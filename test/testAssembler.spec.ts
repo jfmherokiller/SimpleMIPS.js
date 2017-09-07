5c9aa79d8fb9248ae5d211846379608f09ada762
NoahTheGoodra <peterpan0413@live.com>
2017-09-06 21:10:42 -0500

    add arithmatic instructions to test
    Signed-off-by: NoahTheGoodra <peterpan0413@live.com>

--- test/testAssembler.spec.ts
+++ test/testAssembler.spec.ts
@@ -19,37 +19,6 @@ interface opcodeObject {
     instruction: string;
 }
 
-
-function GenerateTest_Instructions() {
-    function range(j, k) {
-        const targetLength = (k - j) + 1;
-        const a = Array(targetLength);
-        const b = Array.apply(null, a);
-        return b.map(function (discard, n) {
-            return n + j;
-        });
-    }
-
-    let operations: opcodeObject[] = [];
-    let memoryaccess_opcodelist: opcodeObject[] = [
-        {instruction: `lb $at,0($at)`},
-        {instruction: `lbu $at,0($at)`},
-        {instruction: `lh $at,0($at)`},
-        {instruction: `lhu $at,0($at)`},
-        {instruction: `lui $at,0`},
-        {instruction: `lw $at,0($at)`},
-        {instruction: `sb $at,0($at)`},
-        {instruction: `sh $at,0($at)`},
-        {instruction: `sw $at,0($at)`},
-        {instruction: `mfhi $at`},
-        {instruction: `mflo $at`},
-        {instruction: `mthi $at`},
-        {instruction: `mtlo $at`},
-    ];
-    operations = operations.concat(memoryaccess_opcodelist);
-    return operations;
-}
-
 describe("Assembler Tests", function () {
     let assembler = new Assembler();
     describe("Assembler initialization", function () {
@@ -58,7 +27,42 @@ describe("Assembler Tests", function () {
         });
     });
     describe("Memory Access Opcodes", function () {
-        GenerateTest_Instructions().forEach(function (opcode) {
+        let memoryaccess_opcodelist: opcodeObject[] = [
+            {instruction: `lb $at,0($at)`},
+            {instruction: `lbu $at,0($at)`},
+            {instruction: `lh $at,0($at)`},
+            {instruction: `lhu $at,0($at)`},
+            {instruction: `lui $at,0`},
+            {instruction: `lw $at,0($at)`},
+            {instruction: `sb $at,0($at)`},
+            {instruction: `sh $at,0($at)`},
+            {instruction: `sw $at,0($at)`},
+            {instruction: `mfhi $at`},
+            {instruction: `mflo $at`},
+            {instruction: `mthi $at`},
+            {instruction: `mtlo $at`},
+        ];
+        memoryaccess_opcodelist.forEach(function (opcode) {
+            it('correctly assembles the opcode ' + opcode.instruction, function () {
+                let assemblout = assembler.assemble(ReturnTestAsm(opcode.instruction)).textMem[2].toString(16);
+                assert.isNotNull(assemblout);
+            });
+        });
+    });
+    describe("Arithmetic Opcodes", function () {
+        let memoryaccess_opcodelist: opcodeObject[] = [
+            {instruction: `add $at,$s1,$s2`},
+            {instruction: `addi $at,5`},
+            {instruction: `addiu $at,10`},
+            {instruction: `addu $at,$s1,$s2`},
+            {instruction: `div $at,$s1,$s2`},
+            {instruction: `divu $at,$s1,$s2`},
+            {instruction: `mult $at,$s1,$s2`},
+            {instruction: `multu $at,$s1,$s2`},
+            {instruction: `sub $at,$s1,$s2`},
+            {instruction: `subu $at,$s1,$s2`},
+        ];
+        memoryaccess_opcodelist.forEach(function (opcode) {
             it('correctly assembles the opcode ' + opcode.instruction, function () {
                 let assemblout = assembler.assemble(ReturnTestAsm(opcode.instruction)).textMem[2].toString(16);
                 assert.isNotNull(assemblout);
