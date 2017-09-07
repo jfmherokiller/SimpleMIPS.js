bdf8ebfa5273607772e9f8c40f92632ca1aef492
NoahTheGoodra <peterpan0413@live.com>
2017-09-06 20:35:01 -0500

    really fix the tests
    Signed-off-by: NoahTheGoodra <peterpan0413@live.com>

--- test/testAssembler.spec.ts
+++ test/testAssembler.spec.ts
@@ -31,7 +31,7 @@ function GenerateTest_Instructions() {
         });
     }
     let operations:opcodeObject[] = [];
-    let GRP_regAliases = ('$zero $at $v0 $v1 $a0 $a1 $a2 $a3 ' +
+    let GRP_regAliases = ('$0 $at $v0 $v1 $a0 $a1 $a2 $a3 ' +
         '$t0 $t1 $t2 $t3 $t4 $t5 $t6 $t7 ' +
         '$s0 $s1 $s2 $s3 $s4 $s5 $s6 $s7 ' +
         '$t8 $t9 $k0 $k1 $gp $sp $fp $ra').split(' ');
