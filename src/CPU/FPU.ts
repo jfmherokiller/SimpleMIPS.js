42b5c8c3f05159cf5086bfed08c62783dcde449f
NoahTheGoodra <peterpan0413@live.com>
2017-09-07 20:01:47 -0500

    added single fpu ops
    Signed-off-by: NoahTheGoodra <peterpan0413@live.com>

--- src/CPU/FPU.ts
+++ src/CPU/FPU.ts
@@ -19,10 +19,20 @@ export class FPU {
         this.registerfile[registerVal] = spitval[0];
         this.registerfile[registerVal-1] = spitval[1];
     }
+    extract_single(registerVal)
+    {
+        return this.registerfile[registerVal];
+    }
+    inject_single(registerVal,value)
+    {
+        this.registerfile[registerVal] = value;
+    }
 
     constructor() {
         this.registerfile = new Float32Array(32);
     }
+
+//double ops
     //rd = rs + rt
     add_double(rd,rt,rs)
     {
@@ -76,5 +86,58 @@ export class FPU {
         let ans = -(arg1);
         this.inject_double(rd,ans);
     }
-
+//single ops
+    //rd = rs + rt
+    add_single(rd,rt,rs)
+    {
+        let arg1 = this.extract_single(rs);
+        let arg2 = this.extract_single(rt);
+        let ans = (arg1 + arg2);
+        this.inject_single(rd,ans);
+    }
+    //rd = rs - rt
+    sub_single(rd,rt,rs)
+    {
+        let arg1 = this.extract_single(rs);
+        let arg2 = this.extract_single(rt);
+        let ans = (arg1 - arg2);
+        this.inject_single(rd,ans);
+    }
+    //rd = rs * rt
+    mul_single(rd,rt,rs)
+    {
+        let arg1 = this.extract_single(rs);
+        let arg2 = this.extract_single(rt);
+        let ans = (arg1 * arg2);
+        this.inject_single(rd,ans);
+    }
+    //rd = rs / rt
+    div_single(rd,rt,rs)
+    {
+        let arg1 = this.extract_single(rs);
+        let arg2 = this.extract_single(rt);
+        let ans = (arg1 / arg2);
+        this.inject_single(rd,ans);
+    }
+    //fd = abs(fs)
+    abs_single(rd,rs)
+    {
+        let arg1 = this.extract_single(rs);
+        let ans = Math.abs(arg1);
+        this.inject_single(rd,ans);
+    }
+    //fd = sqrt(fs)
+    sqrt_single(rd,rs)
+    {
+        let arg1 = this.extract_single(rs);
+        let ans = Math.sqrt(arg1);
+        this.inject_single(rd,ans);
+    }
+    //fd = neg(fs)
+    neg_single(rd,rs)
+    {
+        let arg1 = this.extract_single(rs);
+        let ans = -(arg1);
+        this.inject_single(rd,ans);
+    }
 }
