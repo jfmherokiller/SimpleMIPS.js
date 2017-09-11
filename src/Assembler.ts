061457c0b8a33aa7da6aa8b0e675f2fcc0c6a4fa
NoahTheGoodra <peterpan0413@live.com>
2017-09-11 11:30:17 -0500

    move reg names inside function
    Signed-off-by: NoahTheGoodra <peterpan0413@live.com>

--- src/Assembler.ts
+++ src/Assembler.ts
@@ -901,24 +901,25 @@ export class Assembler {
         return tokenRecognized;
     }
 
-    Float_regAliases = (
-        '$f0,$f1,$f2,$f3,$f4,$f5,$f6,$f7,' +
-        '$f8,$f9,$f10,$f11,$f12,$f13,$f14,$f15,' +
-        '$f16,$f17,$f18,$f19,$f20,$f21,$f22,$f23,' +
-        '$f24,$f25,$f26,$f27,$f28,$f29,$f30,$f31').split(',');
-    GRP_regAliases = ('$zero $at $v0 $v1 $a0 $a1 $a2 $a3 ' +
-        '$t0 $t1 $t2 $t3 $t4 $t5 $t6 $t7 ' +
-        '$s0 $s1 $s2 $s3 $s4 $s5 $s6 $s7 ' +
-        '$t8 $t9 $k0 $k1 $gp $sp $fp $ra').split(' ');
+
 
     convertRegName(regname) {
+        const Float_regAliases = (
+            '$f0,$f1,$f2,$f3,$f4,$f5,$f6,$f7,' +
+            '$f8,$f9,$f10,$f11,$f12,$f13,$f14,$f15,' +
+            '$f16,$f17,$f18,$f19,$f20,$f21,$f22,$f23,' +
+            '$f24,$f25,$f26,$f27,$f28,$f29,$f30,$f31').split(',');
+        const GRP_regAliases = ('$zero,$at,$v0,$v1,$a0,$a1,$a2,$a3,' +
+            '$t0,$t1,$t2,$t3,$t4,$t5,$t6,$t7,' +
+            '$s0,$s1,$s2,$s3,$s4,$s5,$s6,$s7,' +
+            '$t8,$t9,$k0,$k1,$gp,$sp,$fp,$ra,').split(',');
         // GPRs only
         let idx;
         if (regname == 'zero') {
             return 0;
-        } else if ((idx = this.GRP_regAliases.indexOf(regname)) >= 0) {
+        } else if ((idx = GRP_regAliases.indexOf(regname)) >= 0) {
             return idx;
-        } else if ((idx = this.Float_regAliases.indexOf(regname)) >= 0) {
+        } else if ((idx = Float_regAliases.indexOf(regname)) >= 0) {
             return idx;
         } else {
             let match = regname.match(/\d+/),
